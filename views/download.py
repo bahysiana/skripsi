8c4d461972e7c5656ae072bbdb7d8657d5f3acde
bahysiana <bahysiana@gmail.com>
2026-07-13 17:33:20 +0700

    Update download.py

--- views/download.py
+++ views/download.py
@@ -80,6 +80,19 @@ Silakan lakukan proses clustering terlebih dahulu.
     # INFORMASI
     # ======================================================
 
+    st.info(
+        """
+Seluruh hasil analisis dapat diunduh
+sesuai kebutuhan.
+
+• PDF digunakan sebagai laporan hasil analisis.
+
+• Excel digunakan untuk pengolahan data lanjutan.
+
+• CSV digunakan untuk pertukaran data dengan aplikasi lain.
+        """
+    )
+
     st.divider()
 
     # ======================================================
