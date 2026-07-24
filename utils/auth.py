7e73143868b65c7d41ad83c1088e4b2f1a553e6d
bahysiana <bahysiana@gmail.com>
2026-07-24 17:18:23 +0700

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,1 +1,18 @@
+import sqlite3
+import hashlib
 
+from utils.database import DATABASE_PATH
+
+
+# ==========================================================
+# HASH PASSWORD
+# ==========================================================
+
+def hash_password(password: str) -> str:
+    """
+    Mengubah password menjadi SHA-256.
+    """
+
+    return hashlib.sha256(
+        password.encode()
+    ).hexdigest()
