51015ec03bd69f94b2bab00337966ae9f22402bc
bahysiana <bahysiana@gmail.com>
2026-07-24 17:26:42 +0700

    Update login.py

--- views/login.py
+++ views/login.py
@@ -1,1 +1,56 @@
+import streamlit as st
 
+from utils.auth import verify_login
+
+
+# ==========================================================
+# LOGIN PAGE
+# ==========================================================
+
+def show_login():
+
+    if "logged_in" not in st.session_state:
+
+        st.session_state.logged_in = False
+
+    st.title("🔐 Login Admin")
+
+    st.caption(
+        "Sistem Analisis Pola Transaksi Shopee Food"
+    )
+
+    st.divider()
+
+    with st.form("login_form"):
+
+        username = st.text_input(
+            "Username"
+        )
+
+        password = st.text_input(
+            "Password",
+            type="password"
+        )
+
+        login = st.form_submit_button(
+            "Login",
+            use_container_width=True
+        )
+
+    if login:
+
+        if verify_login(username, password):
+
+            st.session_state.logged_in = True
+
+            st.success(
+                "Login berhasil."
+            )
+
+            st.rerun()
+
+        else:
+
+            st.error(
+                "Username atau password salah."
+            )
