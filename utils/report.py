5e493e8875ff524825b0a1ecd40d3cee977beb23
bahysiana <bahysiana@gmail.com>
2026-07-28 08:56:25 +0700

    Update report.py

--- utils/report.py
+++ utils/report.py
@@ -123,7 +123,7 @@ def add_page_number(canvas, doc):
     )
 
     canvas.drawCentredString(
-        17.1 * cm,
+        16.9 * cm,
         A4[1] - 1 * cm,
         "LAPORAN HASIL ANALISIS"
     )
